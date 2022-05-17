09e4ef7acb4f34ce2e7212c93fd71b71f7c81949
Juan Ignacio Martinez <dev@mobilityps.com>
2022-05-17 10:29:24 -0300

    Deleted a library i wasn't using

--- chainWords.py
+++ chainWords.py
@@ -1,9 +1,8 @@
-from tkinter import W
 from networkx import DiGraph, eulerian_circuit
 
 # words = ['chair', 'height', 'racket', 'touch', 'tunic']
 words = ["for", "geek", "rig", "gif", "keeg"]
-print(words[0],words[-1], words)
+# print(words[0],words[-1], words)
 G = DiGraph()
 G.add_weighted_edges_from(((w[0], w[-1], w) for w in words), weight='word')
 result = [G[a][b]['word'] for a,b in eulerian_circuit(G)]
